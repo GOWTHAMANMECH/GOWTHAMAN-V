eed59cb8fbdfe3eae3ee3d35085ea66ae098bf0f
GOWTHAMANMECH <35653073+GOWTHAMANMECH@users.noreply.github.com>
2018-01-26 09:41:59 +0000

    Create 005.c

---
+++ 005.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter the year\n");
+    scanf("%d",&a);
+    if(a%4==0)
+    printf("It is a leap year");
+    else
+    printf("It is not a leap year");
+}
